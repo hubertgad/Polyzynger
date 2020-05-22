16b23f0c6274e662858affc46d9d52c0a11d85f1
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-05-22 09:48:58 +0200

    Update InstallerLOffice.cs

--- XinstApp/Installers/InstallerLOffice.cs
+++ XinstApp/Installers/InstallerLOffice.cs
@@ -17,7 +17,6 @@ namespace XinstApp.Installers
             this.remotePath = "https://download.documentfoundation.org/libreoffice/stable/6.3.6/win/x86_64/LibreOffice_6.3.6_Win_x64.msi"; //TODO: Updater Libre Office
             this.fileName = "LibreOffice_6.3.6_Win_x64.msi";
             this.localPath = Path.Combine(Path.GetTempPath(), this.fileName);
-            this.arguments = "/qn";
             this.Controls.CheckBox.Content = "Libre Office";
         }
         //TODO: Installer Libre Office
