f5cad1dd781a803c598caecd5f279d6e12b9f463
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-08-09 16:23:57 +0200

    Update Installer7Zip.cs

--- XinstApp/Installers/Installers/Installer7Zip.cs
+++ XinstApp/Installers/Installers/Installer7Zip.cs
@@ -17,7 +17,7 @@ namespace XinstApp.Installers
 
         private Installer7Zip() : base()
         {
-            this.remotePath = "https://www.7-zip.org/a/7z1900-x64.msi"; //TODO: Add method which finds lastest version of 7-zip
+            this.remotePath = "https://www.7-zip.org/a/7z1900-x64.msi"; //TODO: Updater 7-zip
             this.fileName = "7z1900-x64.msi";
             this.tempPath = Path.Combine(Path.GetTempPath(), this.fileName);
             this.Controls.CheckBox.Content = "7-Zip";
