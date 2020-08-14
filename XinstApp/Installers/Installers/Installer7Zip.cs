ab84af84f16f721697058e21e0ae9adb23c436f2
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-08-14 11:45:26 +0200

    Update Installer7Zip.cs

--- XinstApp/Installers/Installers/Installer7Zip.cs
+++ XinstApp/Installers/Installers/Installer7Zip.cs
@@ -6,21 +6,31 @@ namespace XinstApp.Installers
     {
         private static Installer7Zip _instance = null;
 
-        public static Installer7Zip Instance
-        {
-            get
-            {
-                if (_instance == null) { _instance = new Installer7Zip(); }
-                return _instance;
-            }
+        public static Installer7Zip Instance 
+        { 
+            get { return _instance ?? _instance = new Installer7Zip(); } 
         }
 
         private Installer7Zip() : base()
         {
-            this.remotePath = "https://www.7-zip.org/a/7z1900-x64.msi"; //TODO: Updater 7-zip
+            this.remotePath = "https://www.7-zip.org/a/7z1900-x64.msi"; //TODO: Check 7-zip EstablishLastestVersionPath()
             this.fileName = "7z1900-x64.msi";
             this.tempPath = Path.Combine(Path.GetTempPath(), this.fileName);
             this.Controls.CheckBox.Content = "7-Zip";
+        }
+        
+        protected override string EstablishLastestVersionPath()
+        {
+            using (WebClient client = new WebClient())
+            {
+                string page = client.DownloadString("https://www.7-zip.org/download.html");
+                string version = Regex.Match(page, "[0-9]{2}[.][0-9]{2}", RegexOptions.IgnoreCase).Value;
+                string versionNo = Regex.Match(version, "[0-9]+[.][0-9]+").Value;
+                versionNo = versionNo.Replace(".", "");
+                this.newRemotePath = this.remotePath.Replace("1900", versionNo);
+                
+                return base.EstablishLastestVersionPath();
+            }
         }
     }
 }
