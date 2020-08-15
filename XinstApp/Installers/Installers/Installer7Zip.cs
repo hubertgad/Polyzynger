39e3c299092793387619d712b86b8c2662208b86
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-08-15 22:07:44 +0200

    usings added, checked EstablishLastestVersionPath working

--- XinstApp/Installers/Installers/Installer7Zip.cs
+++ XinstApp/Installers/Installers/Installer7Zip.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace XinstApp.Installers
 {
@@ -7,19 +9,23 @@ namespace XinstApp.Installers
         private static Installer7Zip _instance = null;
 
         public static Installer7Zip Instance 
-        { 
-            get { return _instance ?? _instance = new Installer7Zip(); } 
+        {
+            get
+            {
+                if (_instance is null) { _instance = new Installer7Zip(); }
+                return _instance;
+            } 
         }
 
         private Installer7Zip() : base()
         {
-            this.remotePath = "https://www.7-zip.org/a/7z1900-x64.msi"; //TODO: Check 7-zip EstablishLastestVersionPath()
+            this.remotePath = "https://www.7-zip.org/a/7z1900-x64.msi";
             this.fileName = "7z1900-x64.msi";
             this.tempPath = Path.Combine(Path.GetTempPath(), this.fileName);
             this.Controls.CheckBox.Content = "7-Zip";
         }
         
-        ///<inheritdoc/>
+        /// <inheritdoc/>
         protected override string EstablishLastestVersionPath()
         {
             using (WebClient client = new WebClient())
